61adffafd4ab24977b68e6c7305c27e8cbb23571
Kent <kent@hcc.hcc>
2025-11-24 09:34:48 -0500

    RecyeAI

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.9ef7048a05e84b3ab2bf8e2dbfaa2fd8',
-  appName: 'OxyAI',
-  webDir: 'dist',
-  server: {
-    url: 'https://9ef7048a-05e8-4b3a-b2bf-8e2dbfaa2fd8.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#0F172A',
-      showSpinner: false,
-    },
-  },
+  appId: 'com.recyeai.app',
+  appName: 'RecyeAI',
+  webDir: 'dist'
 };
 
 export default config;
